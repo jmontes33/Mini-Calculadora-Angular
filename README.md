ceb9197c15035b5c1704e93129580e58fedf9db8
Jose Ramon Montes Hermida <jrmh.business@gmail.com>
2022-12-11 20:39:15 +0100

    Final Commit

--- README.md
+++ README.md
@@ -1,1 +1,31 @@
 # Mini-Calculadora-Angular
+
+## Table of contents
+
+- [Overview](#overview)
+  - [Screenshot](#screenshot)
+  - [Built with](#built-with)
+- [Author](#author)
+
+## Overview
+
+This is a very simple calculator, where you can sum, subtract and multiply two numbers.
+
+Link to the app: https://lucent-torte-a73881.netlify.app/
+
+### Screenshot
+
+![Project finished](./simple-angular-calculator.JPG)
+
+### Built with
+
+- Angular
+- CSS custom properties
+- Flexbox
+- CSS Grid
+- TypeScript
+
+## Author
+
+- TikTok - [@elrincondeldev](https://www.tiktok.com/@elrincondeldev)
+- Instagram - [@elrincondeldev](https://www.instagram.com/elrincondeldev/)
---
+++ src/app/app.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'Mini-Calculadora-Angular';
+  number1: number = 0;
+  number2: number = 0;
+  result: number = 0;
+
+  operations(event: Event): void {
+    if ((<HTMLInputElement> event.target).value == "sum") {
+      this.result = this.number1 + this.number2;
+    } else if ((<HTMLInputElement> event.target).value == "subtract") {
+      this.result = this.number1 - this.number2;
+    } else if ((<HTMLInputElement> event.target).value == "multiply") {
+      this.result = this.number1 * this.number2;
+    }
+  }
+}
